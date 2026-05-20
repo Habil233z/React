ec50cb16f72eba7636a7e053ea74f001e00d0fb2
habil <habilherdianto233z@gmail.com>
2026-05-20 18:41:57 +0700

    solve separate count problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,68 @@
 import { useState } from 'react'
 import './App.css'
-import ProductCard from './components/ProductCard'
 
 function App() {
-    const [hideReduce, setHideReduce] = useState(true)
-
-    const [counter, setCounter] = useState(0)
-
-    const increaseOnclick = () => setCounter(counter + 1 )
-    const decreaseOnclick = () => setCounter(counter - 1 )
-
-  const data = [
+  const [data, setData] = useState([
     {
       id: 1,
       imageSource: "src/assets/image/shoes_1.jpg",
       productName: "productTest 1",
-      productPrice: 100000
+      productPrice: 100000,
+      count: 0
     },
     {
       id: 2,
       imageSource: "src/assets/image/shoes_2.jpg",
       productName: "productTest 2",
-      productPrice: 200000
+      productPrice: 200000,
+      count: 0
     },
     { id: 3,
       imageSource: "src/assets/image/shoes_3.jpg",
       productName: "productTest 3",
-      productPrice: 300000
+      productPrice: 300000,
+      count: 0
     }
-  ]
+  ])
 
-  return (
-    <div className=''>
-      <div className='cardArea'>
-        {data.map(product => <ProductCard 
-          source={product.imageSource}
-          name={product.productName}
-          price={product.productPrice}
-          increaseOnClick={increaseOnclick} decreaseOnClick={decreaseOnclick}
-          hideReduce={counter > 0 ? (false) : (true)} id={product.id} counter={counter}/>)}
-      </div>
+    const increaseOnClick = (id: any) => {
+      const increaseCounter = data.map((count) => {
+        if (count.id === id) {
+          return { ...count, count: count.count + 1}
+        }
+        return count
+      })
+
+      setData(increaseCounter)
+    }
+
+        const decreaseOnClick = (id: any) => {
+      const decreaseCounter = data.map((count) => {
+        if (count.id === id) {
+          return { ...count, count: count.count - 1}
+        }
+        return count
+      })
+
+      setData(decreaseCounter)
+    }
+
+    return (
+      data.map(product => (
+      <div className="productCard">
+        <div className="imageContainer">
+            <img src={product.imageSource} className="productImage" />
+        </div>
+        <h4>{product.productName}</h4>
+        <h3>RP {product.productPrice}</h3>
+        <div className="buyAmount">
+            <button className="increaseButton" onClick={() => increaseOnClick(product.id)}>+</button>
+                <p className= {"product"}>{product.count || 0}</p>
+            <button className="decreaseProduct" onClick={() => decreaseOnClick(product.id)}>-</button>
+        </div>
     </div>
-  )
+      ))
+    )
 }
 
 export default App
